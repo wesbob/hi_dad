83f454d9a54d0bf4d5d2807667b4bbc0e99e2805
wesbob <91146779+wesbob@users.noreply.github.com>
2023-04-18 13:55:21 -0500

    delete function added for image mgmt

--- manage_img.py
+++ manage_img.py
@@ -24,9 +24,24 @@ def add_image_post(title, image_url):
     session.commit()
     print(f"Added new image post: {title}")
 
+def delete_image_post(title):
+    image_post = session.query(ImagePost).filter_by(title=title).first()
+    if image_post:
+        session.delete(image_post)
+        session.commit()
+        print(f"Deleted image post: {title}")
+    else:
+        print("Image post not found")
 
 if __name__ == "__main__":
-    title = input("Enter the image post title: ")
-    image_url = input("Enter the image URL: ")
+    action = input("Do you want to create or delete? (create/delete): ")
 
-    add_image_post(title, image_url)
+    if action.lower() == "create":
+        title = input("Enter the image post title: ")
+        image_url = input("Enter the image URL: ")
+        add_image_post(title, image_url)
+    elif action.lower() == "delete":
+        title = input("Enter the title of the image post to delete: ")
+        delete_image_post(title)
+    else:
+        print("Invalid input. Please enter 'create' or 'delete'.")
